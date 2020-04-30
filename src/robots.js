b0d88ef9736080e53996e1bcebec4a6186f24369
georgesmarandache <georgesmarandache@users.noreply.github.com>
2020-04-30 14:06:12 +0100

    first upload

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+ {
+ 	id: 1,
+ 	name: 'Leanne Graham',
+ 	usenamr: 'LeaG',
+ 	email: 'leag@gmail.com' 
+ },
+ {
+ 	id: 2,
+ 	name: 'Sorin Corhan',
+ 	usenamr: 'SoCo',
+ 	email: 'SorinC@gmail.com' 
+ },
+ {
+ 	id: 3,
+ 	name: 'Mihai Bobo',
+ 	usenamr: 'MBobo',
+ 	email: 'mihaibobo@gmail.com' 
+ },
+ {
+ 	id: 4,
+ 	name: 'Cosmin Alexandru',
+ 	usenamr: 'CosminA',
+ 	email: 'CAlexandru@outlook.com' 
+ },
+ {
+ 	id: 5,
+ 	name: 'Stefan Popa',
+ 	usenamr: 'StafP',
+ 	email: 'StefanPopa@yahoo.com' 
+ },
+ {
+ 	id: 6,
+ 	name: 'Chelsea Graham',
+ 	usenamr: 'ChelseaG',
+ 	email: 'chelsea@outlook.com' 
+ },
+ {
+ 	id: 7,
+ 	name: 'Poppy Michaels',
+ 	usenamr: 'PoppyM',
+ 	email: 'PoppyM@gmail.com' 
+ }
+]
